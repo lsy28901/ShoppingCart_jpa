e0665c05b5e9ce8ef04209fab4048f2bfd831dfe
lsy28901 <tndus28901@gmail.com>
2024-09-11 11:09:56 +0900

    🔨refactor/ @ToString 추가

--- src/main/java/com/supercoding/mall_jpa/entity/User.java
+++ src/main/java/com/supercoding/mall_jpa/entity/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor
+@ToString(exclude = {"id","cart"})
 public class User {
 
     @Id
